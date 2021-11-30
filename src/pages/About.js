13763837e2799a8de2dd748004099ff7f7309240
FalconHD <youssbak.2015@gmail.com>
2021-11-30 14:19:09 +0100

    components done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,3 @@
 export const About = () => {
-    console.log("abouuuuuuuuuuuuut page");
-    document.querySelector('#ctn').innerHTML = 'about'
+    document.querySelector('#ctn').innerHTML = 'about Page'
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
+import { image } from '../components/image'
 export const Home = () => {
-    console.log("we are in home bitchhhhhhhh");
-
-    document.querySelector('#ctn').innerHTML = 'home'
+    document.querySelector('#ctn').innerHTML = `
+        <h1>Home page</h1>
+        ${image()}
+    `
 }
