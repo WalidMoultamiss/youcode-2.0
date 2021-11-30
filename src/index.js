5a981fbabced197f737e9f6d64a881affb4a72bf
FalconHD <youssbak.2015@gmail.com>
2021-11-30 14:52:47 +0100

    return

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ import { render, router } from "./helpers"
 
 export default function app() {
   const goTo = (path) => {
+    console.log("path", path);
     history.pushState({ usreid: 3 }, path, "/?page=" + path)
     router()
-    let page = pages.find(page => page.path.toLowerCase() === path)
+    let page = pages.find(page => {
+      let checker = path === "/" ? path : path.replace('/', "")
+      return page?.path.toLowerCase() === checker
+    })
     render(page)
   }
+  window.onpopstate = () => {
+    router()
+  }
+
+  router()
+  goTo(location.pathname)
   window.goTo = goTo
 }
