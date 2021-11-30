20243862d67083413a17d57e11e33fbf8b462591
FalconHD <youssbak.2015@gmail.com>
2021-11-30 14:09:34 +0100

    router done

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 import './style.css'
 import "tailwindcss/tailwind.css"
-import { render, router } from "./src/helpers"
-import { pages } from './src'
-
-console.log(pages);
-// router()
+import app from './src'
 
 
-const goTo = (path) => {
-    history.pushState({ usreid: 3 }, path, "/?page=" + path)
-    router()
-    let page = pages.find(page => page.path.toLowerCase() === path)
-    console.log(pages);
-    render(page)
-}
 
-window.goTo = goTo
-
-
+app()
 
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import { } from './classes'
 import { } from './helpers'
 import { } from './components'
 import pages from './pages'
+import { render, router } from "./helpers"
 
 
-
-export  {
-  pages
+export default function app() {
+  const goTo = (path) => {
+    history.pushState({ usreid: 3 }, path, "/?page=" + path)
+    router()
+    let page = pages.find(page => page.path.toLowerCase() === path)
+    render(page)
+  }
+  window.goTo = goTo
 }
