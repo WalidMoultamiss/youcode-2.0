489225f1d77ba33af51ffcc73ba76166ca4d8c45
walid moultamiss <walidmoultamis@gmail.com>
2021-11-30 10:11:48 +0100

    setup ready

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import "tailwindcss/tailwind.css"
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="text-red-300">Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
