69934d0881cfdb1c90943390ab23405132f6342c
walid moultamiss <walidmoultamis@gmail.com>
2021-12-01 11:16:54 +0100

    add dark mode to login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -54,7 +54,7 @@ export const header = (data) => {
                                   user ? user[0].fullName : null
                                 }</p>
                             </div>
-                            <img class="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/200/300" alt="Avatar of Jonathan Reinink">
+                            <img class="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/200/300" alt="Avatar of Youcode user">
                             <button onclick="toggleDarkMode()" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
                                 dark
                             </button>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,35 +1,37 @@
-
-
-
-
 export const login = () => {
-    
-    //get info from local storage
-    const user = {
-        email: localStorage.getItem('email'),
-        password: localStorage.getItem('password')
-    }
+  //get info from local storage
+  const user = {
+    email: localStorage.getItem("email"),
+    password: localStorage.getItem("password"),
+  };
 
-
-
-
-    return (`
-        <div class="flex flex-col items-center justify-center h-screen">
-            <div class="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+  return `
+        <div class="flex dark:bg-gray-900 flex-col items-center justify-center h-screen">
+            <div class="bg-white dark:bg-gray-700 shadow-lg rounded-lg ">
+                <div class="w-full p-3">
+                    <div class="flex justify-start gap-3 items-center">
+                        <div class="p-2 rounded-full cursor-pointer bg-red-600"></div>
+                        <div class="p-2 rounded-full cursor-pointer bg-yellow-400"></div>
+                        <div class="p-2 rounded-full cursor-pointer bg-green-400" ></div>
+                        <input type="text" class="w-full bg-gray-200 outline-none p-1 text-xs px-3 rounded-lg" id="url" placeholder="url" value="https://www.youcode2.ma">
+                    </div>
+                </div>
+                <hr/>
+            <div class="px-8 pt-6 pb-8 mb-4">
                 <div class="mb-4">
-                    <h1 class="text-2xl font-bold text-center">Login</h1>
+                    <h1 class="text-2xl font-bold dark:text-white text-center">Login</h1>
                 </div>
                 <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    <label class="block text-gray-700 dark:text-white text-sm font-bold mb-2" for="email">
                         Email
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" value="${user.email}">
+                    <input class="shadow appearance-none dark:bg-gray-600 border rounded w-full py-2 px-3 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" value="${user.email}">
                 </div>
                 <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    <label class="block text-gray-700 dark:text-white text-sm font-bold mb-2" for="password">
                         Password
                     </label>
-                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" value="${user.password}" type="password" placeholder="******************">
+                    <input class="shadow appearance-none border dark:bg-gray-600 border-red-500 rounded w-full py-2 px-3 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" value="${user.password}" type="password" placeholder="******************">
                     <p class="text-red-500 text-xs italic">Please choose a password.</p>
                 </div>
                 <div class="flex items-center justify-between">
@@ -40,6 +42,8 @@ export const login = () => {
                         Forgot Password?
                     </a>
                 </div>
+                </div>
             </div>
         </div>
-      `)}
+      `;
+};
