18f8852ad7edf1bb22938fe28aec1b60663931c1
walid moultamiss <walidmoultamis@gmail.com>
2021-12-02 17:38:58 +0100

    fix put quest

--- src/pages/viewRes.js
+++ src/pages/viewRes.js
@@ -5,16 +5,26 @@ export const viewRes = () => {
         const status = res.status
         const questions = res.testOnline
         console.log(questions);
-        return res.status
+        return {
+            status:()=> status,
+            questions:()=> questions[0],
+        }
     }
+
+    // const renderQuestions = ()=>{
+    //     const questions = getRes()
+
 
 
 
     return `
     <div class="w-full h-screen pt-44 dark:bg-gray-900">
         <h1 class="dark:text-white">
-            ${getRes()}
+            ${getRes().status()}
         </h1>
+        <h2 class="dark:text-white">
+        ${getRes().questions().isCorrect}
+        </h2>
     </div>
     `
 }
